3d6ec119b79ea31bfdb0216b9a81c0bebb97c41d
Dove <codegeekdove@gmail.com>
2026-08-04 15:06:33 +0800

    Added the checklist data from context

--- src/Components/PackingList/index.tsx
+++ src/Components/PackingList/index.tsx
@@ -1,14 +1,16 @@
 import styles from './style.module.scss'
 
 import type { JSX } from 'react/jsx-runtime'
-import checkListData, { type BoardingChecklist } from '../../assets/data'
+import { type BoardingChecklist } from '../../assets/data'
+import { useCheckList } from '../../contexts/ChecklistProvider'
 import PackingItem from '../PackingItem'
 
 export default function PackingList() {
+  const { checkList } = useCheckList()
   return (
     <section className={styles.packinglist}>
       <ul className={styles.packinglist__container}>
-        {checkListData.map(
+        {checkList.map(
           (list: BoardingChecklist): JSX.Element => (
             <PackingItem key={list.id} list={list} styles={styles} />
           ),
