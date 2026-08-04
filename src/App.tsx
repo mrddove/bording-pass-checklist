fe68173cdf9df87876ad357298630906300952c1
Dove <codegeekdove@gmail.com>
2026-08-04 15:02:04 +0800

    Wrapped component in App.tsx to context provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import Footer from './Components/Footer'
 import Header from './Components/Header'
 import PackingForm from './Components/PackingForm'
 import PackingList from './Components/PackingList'
+import { ChecklistProvider } from './contexts/ChecklistProvider'
 
 function App() {
   return (
-    <>
+    <ChecklistProvider>
       <Header />
       <PackingForm />
       <PackingList />
       <Footer />
-    </>
+    </ChecklistProvider>
   )
 }
 
