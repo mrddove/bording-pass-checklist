689d8516d174735acef67a6cb6bfd42b6a0da148
Dove <codegeekdove@gmail.com>
2026-08-12 09:49:55 +0800

    refactor SelectField component

--- src/Components/SelectField/index.tsx
+++ src/Components/SelectField/index.tsx
@@ -6,18 +6,22 @@ type Options<T> = {
 
 type SelecFieldProps<T> = {
   label: string
-  name: string
-  styles: Record<string, string>
+  name?: string
+  value?: string
+  styles: string
   options: Options<T>[] // it will use it here
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
 }
 
 export default function SelecField<T extends string>({
   name,
   styles,
   options,
+  onChange,
+  value,
 }: SelecFieldProps<T>) {
   return (
-    <select name={name} className={styles.listbar__select}>
+    <select name={name} value={value} className={styles} onChange={onChange}>
       {options.map((option) => (
         <option key={option.value} value={option.value}>
           {option.text}
